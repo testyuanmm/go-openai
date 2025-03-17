7c17118eacb8460756d178de80aa35d6eef5d5d8
testyuanmm <30712974+testyuanmm@users.noreply.github.com>
2025-03-17 09:51:12 +0800

    Update embeddings.go

--- embeddings.go
+++ embeddings.go
@@ -181,6 +181,7 @@ type EmbeddingRequestStrings struct {
 	Model EmbeddingModel `json:"model"`
 	// A unique identifier representing your end-user, which will help OpenAI to monitor and detect abuse.
 	User string `json:"user"`
+	Prompt string `json:"prompt"`
 	// EmbeddingEncodingFormat is the format of the embeddings data.
 	// Currently, only "float" and "base64" are supported, however, "base64" is not officially documented.
 	// If not specified OpenAI will use "float".
